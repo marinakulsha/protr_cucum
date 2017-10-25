07bad3e26fc2c570cfbf81116d78bb754a2e0bf6
Marina <marina_kul@tut.by>
2017-10-25 15:10:46 +0300

    modify Page Factory

--- test/step_difinitions/steps.js
+++ test/step_difinitions/steps.js
@@ -1,26 +1,26 @@
 let {defineSupportCode} = require('cucumber');
-let PageFactory = require('./../support/pages/PageFactory').getInstance().getPage('home');
+let PageFactory = require('./../support/pages/PageFactory');
 
 defineSupportCode(function ({Given, When, Then}) {
 
     When(/^I click '(.+)' element$/, function (name) {
-        return PageFactory.clickButton(name)
+        return PageFactory.getInstance().getPage('home').clickButton(name)
     });
 
     When(/^I press '(.+)' button$/, function (name) {
 
-        return PageFactory.pressButton(name)
+        return PageFactory.getInstance().getPage('home').pressButton(name)
     });
 
     When(/^I input into '(.+)' field the value '(.+)'$/, function (name, title) {
 
-        return PageFactory.inputValue(name, title)
+        return PageFactory.getInstance().getPage('home').inputValue(name, title)
     });
 
 
     Then(/^I should see '(.+)' element on page$/, function (name) {
 
-        return PageFactory.elementVisibility(name)
+        return PageFactory.getInstance().getPage('home').elementVisibility(name)
             .then(
                 (result) => {
                     if (result) {

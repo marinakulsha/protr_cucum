029a45b9298ddf7f6046c40c7e55197574f6a885
Marina <marina_kul@tut.by>
2017-11-09 12:30:05 +0300

    request.js added

---
+++ request.js
@@ -0,0 +1,11 @@
+let request= require('request')
+ request('http://foodbank.d5.aisnovations.com/', function(err, response,body){
+     if(err){
+         throw Err(err);
+     }
+     console.log(response.statusCode);
+     console.log(response.headers['content-type']);
+     console.log(response.headers['cache-control']);
+     console.log(response.headers)
+
+ });
